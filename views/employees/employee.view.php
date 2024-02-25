2ed3952d67fa1c8fe1a3f6795263179ba3ffbadb
TIV.NGUOT <TIV.NGUOT@PNC.LAN>
2024-02-25 10:59:54 +0700

    [feat] Display employees in the table LMS-17

--- views/employees/employee.view.php
+++ views/employees/employee.view.php
@@ -9,15 +9,17 @@
                 <div class="row align-items-center">
                     <!-- search box -->
                     <div class="col-md-6 mb-2">
-                        <input type="text" name="search_employee" id="search_employee" placeholder="Search employee..." class="form-control" />
+                        <input type="text" name="search_employee" id="search_employee" placeholder="Search employee..."
+                            class="form-control" />
                     </div>
                     <!-- filter -->
                     <div class="col-md-4 mb-2">
-                        <select id="position" name="position" class="custom-select  border-dark" tile="Please select the position of the employee." required>
+                        <select id="position" name="position" class="custom-select  border-dark"
+                            tile="Please select the position of the employee." required>
                             <option value="" selected disabled>Choose Position</option>
                             <?php $get_positions = get_positions(); ?>
                             <?php foreach ($get_positions as $position) :  ?>
-                                <option value="<?= $position['id']; ?>"><?= $position['position']; ?></option>
+                            <option value="<?= $position['id']; ?>"><?= $position['position']; ?></option>
                             <?php endforeach; ?>
                         </select>
                     </div>
@@ -39,54 +41,28 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <?php
+                    // call the function to retrieve all employees from the database
+                    $get_employees_with_positions = get_employees_with_positions();
+                    ?>
+                    <?php foreach ($get_employees_with_positions as $employee) : ?>
                     <tr class="border-bottom" style="font-size:14px">
                         <td class="d-flex" style="text-align: center; vertical-align: middle;">
-                            <img style="width: 60px; object-fit: cover;" class="shadow-none rounded-circle" alt="avatar1" src="https://www.allkpop.com/upload/2024/01/content/102324/1704947068-20240110-iu.jpg" />
-                            <span class="mt-3 m-lg-3">Tanner Hickman</span>
+                            <img style="width: 60px; object-fit: cover;" class="shadow-none rounded-circle"
+                                alt="avatar1"
+                                src="<?= 'data:image/jpeg;base64,' . base64_encode($employee['image_data']) ?>" />
+                            <span
+                                class="mt-3 m-lg-3"><?= $employee['first_name'] . " " . $employee['last_name'] ?></span>
                         </td>
-                        <td style="vertical-align: middle;">sifilycoza@mailinator.com</td>
-                        <td style="vertical-align: middle;">Developer</td>
+                        <td style="vertical-align: middle;"><?= $employee['email'] ?></td>
+                        <td style="vertical-align: middle;"><?= $employee['position'] ?></td>
                         <td style="vertical-align: middle;">
                             <a href="#" class="btn  btn-success" style="font-size:13px">Update</a>
-                            <a href="#" class="btn btn-danger border border-0" style="font-size:13px">Delete</a>
+                            <a href="#" onclick="openDeleteModal(<?php echo $employee['id']; ?>)"
+                                class="btn btn-danger border border-0" style="font-size:13px">Delete</a>
                         </td>
                     </tr>
-                    <tr class="border-bottom" style="font-size:14px">
-                        <td class="d-flex" style="text-align: center; vertical-align: middle;">
-                            <img style="width: 60px; object-fit: cover;" class="shadow-none rounded-circle" alt="avatar1" src="https://t.ly/E7wHA" />
-                            <span class="mt-3 m-lg-3">Marshall Roberson</span>
-                        </td>
-                        <td style="vertical-align: middle;">toligir@mailinator.com</td>
-                        <td style="vertical-align: middle;">Marketing</td>
-                        <td style="vertical-align: middle;">
-                            <a href="#" class="btn  btn-success" style="font-size:13px">Update</a>
-                            <a href="#" class="btn btn-danger border border-0" style="font-size:13px">Delete</a>
-                        </td>
-                    </tr>
-                    <tr class="border-bottom" style="font-size:14px">
-                        <td class="d-flex" style="text-align: center; vertical-align: middle;">
-                            <img style="width: 60px; object-fit: cover;" class="shadow-none rounded-circle" alt="avatar1" src="https://media.asiaone.com/sites/default/files/styles/article_main_image/public/original_images/May2023/230525_ma%20dong%20seok.png?itok=4MFBNauv" />
-                            <span class="mt-3 m-lg-3">Justina Mendoza</span>
-                        </td>
-                        <td style="vertical-align: middle;">zizazuxaxo@mailinator.com</td>
-                        <td style="vertical-align: middle;">QA</td>
-                        <td style="vertical-align: middle;">
-                            <a href="#" class="btn  btn-success" style="font-size:13px">Update</a>
-                            <a href="#" class="btn btn-danger border border-0" style="font-size:13px">Delete</a>
-                        </td>
-                    </tr>
-                    <tr class="" style="font-size:14px">
-                        <td class="d-flex" style="text-align: center; vertical-align: middle;">
-                            <img style="width: 60px; object-fit: cover;" class="shadow-none rounded-circle" alt="avatar1" src="https://0.soompi.io/wp-content/uploads/2023/03/22100129/Ahn-Jae-Hyun-4-1.jpg" />
-                            <span class="mt-3 m-lg-3">Justina Mendoza</span>
-                        </td>
-                        <td style="vertical-align: middle;">zizazuxaxo@mailinator.com</td>
-                        <td style="vertical-align: middle;">QA</td>
-                        <td style="vertical-align: middle;">
-                            <a href="#" class="btn  btn-success" style="font-size:13px">Update</a>
-                            <a href="#" class="btn btn-danger border border-0" style="font-size:13px">Delete</a>
-                        </td>
-                    </tr>
+                    <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
