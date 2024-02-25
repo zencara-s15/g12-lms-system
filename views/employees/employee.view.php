76d8848f0286bc859637fed1f181139fbcb5c420
TIV.NGUOT <TIV.NGUOT@PNC.LAN>
2024-02-25 13:42:46 +0700

    [feat] delete employee LMS-18

--- views/employees/employee.view.php
+++ views/employees/employee.view.php
@@ -9,17 +9,15 @@
                 <div class="row align-items-center">
                     <!-- search box -->
                     <div class="col-md-6 mb-2">
-                        <input type="text" name="search_employee" id="search_employee" placeholder="Search employee..."
-                            class="form-control" />
+                        <input type="text" name="search_employee" id="search_employee" placeholder="Search employee..." class="form-control" />
                     </div>
                     <!-- filter -->
                     <div class="col-md-4 mb-2">
-                        <select id="position" name="position" class="custom-select  border-dark"
-                            tile="Please select the position of the employee." required>
+                        <select id="position" name="position" class="custom-select  border-dark" tile="Please select the position of the employee." required>
                             <option value="" selected disabled>Choose Position</option>
                             <?php $get_positions = get_positions(); ?>
                             <?php foreach ($get_positions as $position) :  ?>
-                            <option value="<?= $position['id']; ?>"><?= $position['position']; ?></option>
+                                <option value="<?= $position['id']; ?>"><?= $position['position']; ?></option>
                             <?php endforeach; ?>
                         </select>
                     </div>
@@ -46,25 +44,60 @@
                     $get_employees_with_positions = get_employees_with_positions();
                     ?>
                     <?php foreach ($get_employees_with_positions as $employee) : ?>
-                    <tr class="border-bottom" style="font-size:14px">
-                        <td class="d-flex" style="text-align: center; vertical-align: middle;">
-                            <img style="width: 60px; object-fit: cover;" class="shadow-none rounded-circle"
-                                alt="avatar1"
-                                src="<?= 'data:image/jpeg;base64,' . base64_encode($employee['image_data']) ?>" />
-                            <span
-                                class="mt-3 m-lg-3"><?= $employee['first_name'] . " " . $employee['last_name'] ?></span>
-                        </td>
-                        <td style="vertical-align: middle;"><?= $employee['email'] ?></td>
-                        <td style="vertical-align: middle;"><?= $employee['position'] ?></td>
-                        <td style="vertical-align: middle;">
-                            <a href="#" class="btn  btn-success" style="font-size:13px">Update</a>
-                            <a href="#" onclick="openDeleteModal(<?php echo $employee['id']; ?>)"
-                                class="btn btn-danger border border-0" style="font-size:13px">Delete</a>
-                        </td>
-                    </tr>
+                        <tr class="border-bottom" style="font-size:14px">
+                            <td class="d-flex" style="text-align: center; vertical-align: middle;">
+                                <img style="width: 60px; object-fit: cover;" class="shadow-none rounded-circle" alt="avatar1" src="<?= 'data:image/jpeg;base64,' . base64_encode($employee['image_data']) ?>" />
+                                <span class="mt-3 m-lg-3"><?= $employee['first_name'] . " " . $employee['last_name'] ?></span>
+                            </td>
+                            <td style="vertical-align: middle;"><?= $employee['email'] ?></td>
+                            <td style="vertical-align: middle;"><?= $employee['position'] ?></td>
+                            <td style="vertical-align: middle;">
+                                <a href="#" class="btn  btn-success" style="font-size:13px">Update</a>
+                                <a href="#" onclick="openDeleteModal(<?php echo $employee['id']; ?>)" class="btn btn-danger border border-0" style="font-size:13px">Delete</a>
+                            </td>
+                        </tr>
                     <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
     </div>
 </div>
+<!-- Delete Confirmation Modal -->
+<div class="modal fade" id="confirmDeleteModal" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmDeleteModalLabel">Confirm Delete</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Are you sure you want to delete this employee?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-danger border border-0 " id="confirmDeleteBtn">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    // JavaScript function to open the delete confirmation modal
+    function openDeleteModal(employeeId) {
+        // Store the employeeId in the delete button's data attribute
+        document.getElementById('confirmDeleteBtn').dataset.employeeId = employeeId;
+
+        // Open the modal
+        $('#confirmDeleteModal').modal('show');
+    }
+
+    // Event listener for the delete button in the modal
+    document.getElementById('confirmDeleteBtn').addEventListener('click', function() {
+        // Get the employeeId from the data attribute
+        var employeeId = this.dataset.employeeId;
+
+        // Redirect to the delete URL
+        window.location.href = "/controllers/employees/delete.employee.controller.php?id=" + employeeId;
+    });
+</script>
