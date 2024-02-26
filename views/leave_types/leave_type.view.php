642738afc7b45004abd77caf2bd01c8f77596058
LAYHOR.MET <LAYHOR.MET@PNC.LAN>
2024-02-26 10:51:09 +0700

    [imp]update on delete to confirm

--- views/leave_types/leave_type.view.php
+++ views/leave_types/leave_type.view.php
@@ -54,7 +54,7 @@
 											<a href="/edit_leave_type?id=<?= $leave_type['id'] ?>" class="btn btn-theme ctm-border-radius text-white " data-placement="bottom" style="height:40px" title="Edit"><i class="fa fa-pencil"></i></a>
 										</span>
 										<span data-toggle="modal" data-target="#delete">
-											<a href="../../controllers/leave_types/form_delete.controller.php?id=<?= $leave_type['id'] ?>" class="btn btn-theme ctm-border-radius text-white" data-placement="bottom" style=" height:40px" title="Delete"><i class="fa fa-trash"></i></a>
+										<a href="#" onclick="openDeleteModal(<?php echo $leave_type['id']; ?>)" class="btn btn-theme ctm-border-radius text-white" data-placement="bottom" style="height:40px" title="Delete"><i class="fa fa-trash"></i></a>
 										</span>
 									</div>
 								</td>
@@ -66,3 +66,59 @@
 		</div>
 	</div>
 </div>
+
+<div class="modal fade" id="confirmDeleteModal" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="confirmDeleteModalLabel">Confirm Delete</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						Are you sure you want to delete this leave_type?
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-theme ctm-border-radius text-white" data-dismiss="modal">Cancel</button>
+						<button type="button" class="btn btn-theme ctm-border-radius text-white" id="confirmDeleteBtn">Delete</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Delete Confirmation Modal -->
+	<div class="modal fade" id="confirmDeleteModal" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div>
+					Are you sure you want to delete this employee?
+				</div>
+				<div>
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+					<button type="button" class="btn btn-danger border border-0 " id="confirmDeleteBtn">Delete</button>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script>
+		function openDeleteModal(employeeId) {
+			document.getElementById('confirmDeleteBtn').dataset.employeeId = employeeId;
+			// Open the modal
+			$('#confirmDeleteModal').modal('show');
+		}
+		document.getElementById('confirmDeleteBtn').addEventListener('click', function() {
+			let employeeId = this.dataset.employeeId;
+			// Redirect to the delete URL
+			window.location.href = "/controllers/leave_types/form_delete.controller.php?id=" + employeeId;
+		});
+	</script>
